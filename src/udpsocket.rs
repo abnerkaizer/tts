dc00e895b29fc5c2e407e80c89a26e750785cd1f
Abner Kaizer <abnerkaizer@protonmail.com>
2021-02-03 13:06:40 -0300

    .

--- src/udpsocket.rs
+++ src/udpsocket.rs
@@ -10,8 +10,17 @@ pub fn run() {
             std::process::exit(1);
         }
     };
+    //socket.connect("localhost:6790").expect("connect function failed");
     let mut content = [0;1024];
-    socket.recv(&mut content).expect("Failed to receive.");
+    match socket.recv(&mut content){
+        Ok(received) => {
+            &content[..received];
+        },
+        Err(e) =>{
+            eprintln!("Couldn't receive content: {}", e);
+            std::process::exit(1);
+        }
+    };
     let content = std::str::from_utf8(&content).expect("Failed to parse.");
     println!("{}",content);
 }
