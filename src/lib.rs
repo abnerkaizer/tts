533015476901ed25e4e9ea428c24371671d47138
Abner Kaizer <abnerkaizer@protonmail.com>
2021-02-03 11:24:53 -0300

    Versioning

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod udpsocket;
 
 pub fn run() {
     App::new("Text Transfer")
-        .version("0.1.1")
+        .version("0.1.2")
         .author("Abner K. <abnerkaizer@protonmail.com>")
         .about("Transfers a text file through UDP or TCP. UDP is the default one.")
         .get_matches();
